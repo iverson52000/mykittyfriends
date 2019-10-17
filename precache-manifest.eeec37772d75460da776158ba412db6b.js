c59bc752db2eb8b4b1c71bf160c2cb785368da01
Albert Hsu <iverson52000@gmail.com>
2019-10-17 14:17:05 -0700

    Updates

---
+++ precache-manifest.eeec37772d75460da776158ba412db6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13ef175d177a3b32d46895d715cba824",
+    "url": "/mykittyfriends/index.html"
+  },
+  {
+    "revision": "12433437fab293809f9c",
+    "url": "/mykittyfriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "9d64a58920ee30c6bac0",
+    "url": "/mykittyfriends/static/css/main.26244fbb.chunk.css"
+  },
+  {
+    "revision": "12433437fab293809f9c",
+    "url": "/mykittyfriends/static/js/2.8cf85a55.chunk.js"
+  },
+  {
+    "revision": "9d64a58920ee30c6bac0",
+    "url": "/mykittyfriends/static/js/main.138b61b1.chunk.js"
+  },
+  {
+    "revision": "f37062f78d8fe43d3a3b",
+    "url": "/mykittyfriends/static/js/runtime-main.3cdced70.js"
+  }
+]);
